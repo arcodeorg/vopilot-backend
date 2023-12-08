c3a3577c6da2fc9409e09f788d306149a4884cdd
Jack Arana <arack.dev@gmail.com>
2023-12-08 16:41:47 -0500

    feat(shared): add AuditableModel for entity auditing

---
+++ src/main/java/com/arcode/vopilot/shared/domain/entities/AuditableModel.java
@@ -0,0 +1,22 @@
+package com.arcode.vopilot.shared.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Data
+@MappedSuperclass
+public class AuditableModel {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+}
