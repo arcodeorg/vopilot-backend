2975cc44ea674c149091ca689e941ebe350c7a52
Jack Arana <arack.dev@gmail.com>
2023-12-08 15:56:23 -0500

    Initial commit

---
+++ src/main/java/com/arcode/vopilot/VopilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arcode.vopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VopilotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VopilotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcode/vopilot/VopilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcode.vopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VopilotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
