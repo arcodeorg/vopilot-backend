054dcff57b32114da13a1c3550df795fc61a8ea9
Wilver Arana R <u202023992@upc.edu.pe>
2023-12-09 14:23:36 -0500

    feat(Shared): add MapperConfig and Dependency

---
+++ src/main/java/com/arcode/vopilot/shared/infrastructure/mapper/MapperConfig.java
@@ -0,0 +1,16 @@
+package com.arcode.vopilot.shared.infrastructure.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // This annotation indicates that this class contains one or more bean methods annotated with @Bean producing beans manageable by spring container.
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
